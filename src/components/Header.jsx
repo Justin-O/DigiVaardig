8b9358475ecc6629532c2441d6196ffa09d23ef1
Justin-O <justinplaystation@live.nl>
2024-05-05 03:33:30 +0200

    header improvements

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,21 +3,42 @@ import { FaAngleLeft } from "react-icons/fa6";
 import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 
+import { useState, useEffect } from "react";
+
 function Header(props) {
+	const [isScrolled, setIsScrolled] = useState(false);
+
+	useEffect(() => {
+		const handleScroll = () => {
+			const scrollTop = window.scrollY;
+			setIsScrolled(scrollTop > 96);
+		};
+
+		window.addEventListener("scroll", handleScroll);
+		return () => {
+			window.removeEventListener("scroll", handleScroll);
+		};
+	}, []);
+
 	return (
-		<header className="flex flex-col fixed w-full">
-			<div className="flex justify-between items-center bg-black w-full h-24 px-4 md:px-8 z-50">
-				<Link className="flex items-center h-full" to={"/"}>
+		<header className={`${props.subheader ? "relative" : "fixed"} flex flex-col w-full`}>
+			<div
+				className={`${props.subheader && isScrolled ? "hidden" : "flex absolute top-0 justify-between items-center bg-black w-full h-24 px-4 md:px-8"}`}>
+				<Link
+					className={`${props.subheader && isScrolled ? "hidden" : "flex items-center h-full"}`}
+					to={"/"}>
 					<h1 className="text-2xl md:text-3xl text-white font-normal">
 						dbieb - <span className="font-bold text-red">DigiVaardig</span>
 					</h1>
 				</Link>
-				<button className="flex justify-center items-center rounded-full w-11 h-11 bg-white text-3xl font-bold">
+				<button
+					className={`${props.subheader && isScrolled ? "hidden" : "flex"}  justify-center items-center rounded-full w-11 h-11 bg-white text-3xl font-bold`}>
 					?
 				</button>
 			</div>
 			{props.subheader && (
-				<div className="flex items-center bg-darkgrey w-full h-16 px-4 md:px-8">
+				<div
+					className={`${isScrolled ? "fixed" : "absolute top-24"} flex items-center bg-darkgrey w-full h-16 px-4 md:px-8`}>
 					<div className="flex justify-start w-1/3 md:w-1/5">
 						<Link
 							to={props.backButtonPath}
@@ -28,6 +49,12 @@ function Header(props) {
 					</div>
 					<div className="flex justify-end md:justify-center w-2/3 md:w-3/5">
 						<h2 className="text-white text-lg md:text-2xl font-semibold text-end md:text-center max-h-16 overflow-hidden">
+							{props.chapterId && (
+								<>
+									<span className="text-red">{props.chapterId}</span>
+									{" - "}
+								</>
+							)}
 							{props.headerTitle}
 						</h2>
 					</div>
@@ -41,6 +68,7 @@ function Header(props) {
 Header.propTypes = {
 	subheader: PropTypes.bool,
 	backButtonPath: PropTypes.string,
+	chapterId: PropTypes.number,
 	headerTitle: PropTypes.string,
 };
 
