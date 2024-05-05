6f5e6b119a5a07ffb5015cfaaac3d942650c89bb
Justin-O <justinplaystation@live.nl>
2024-05-05 16:36:55 +0200

    assignment description dynamic loading

--- src/pages/AssignmentDescriptionPage.jsx
+++ src/pages/AssignmentDescriptionPage.jsx
@@ -1,30 +1,39 @@
-import { Link, useLocation } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import styles from "../styles/global.module.css";
 import Header from "../components/Header";
+import { object } from "prop-types";
 
-function AssignmentDescriptionPage() {
-	const currentLocation = useLocation();
+function AssignmentDescriptionPage({ contentData }) {
+	const { chapterURL, assignmentURL } = useParams();
+	const currentChapterData = contentData.chapters.find(
+		(chapter) => chapter.chapterURL === chapterURL,
+	);
+	const currentAssignmentData = currentChapterData.assignments.find(
+		(assignment) => assignment.assignmentURL === assignmentURL,
+	);
+
+	const descriptionParagraphs = currentAssignmentData.assignmentDescription.split("\n\n");
 
 	return (
 		<>
-			<Header subheader headerTitle={"Placeholder Opdracht titel"} backButtonPath={"../"} />
+			<Header
+				subheader
+				headerTitle={currentAssignmentData.assignmentTitle}
+				backButtonPath={"../"}
+			/>
 			<div className="flex w-full justify-center">
-				<div className="flex flex-col justify-center gap-12 px-6 cards:px-0 pb-16 pt-48 md:pt-52 w-full max-w-3xl min-h-screen">
-					<p className="min-h-[35vh] text-lg md:text-xl">
-						In deze oefening wordt stapsgewijs uitgelegd hoe u de tekstopmaak van uw
-						e-mails kunt aanpassen, inclusief het wijzigen van lettertypen, groottes en
-						kleuren. Door deze vaardigheden te leren, kunt u niet alleen uw e-mails
-						personaliseren, maar ook de leesbaarheid verbeteren en uw boodschap
-						effectiever overbrengen. <br />
-						<br />
-						Of u nu wilt opvallen met creatieve kleuren en stijlen of een professionele
-						uitstraling wilt behouden, deze oefening biedt de tools die u nodig hebt om
-						uw e-mailcommunicatie naar een hoger niveau te tillen.
-					</p>
-					<div className="sticky bottom-0 bg-sand py-4 md:py-6 z-10">
+				<div className="flex flex-col justify-center gap-6 px-6 cards:px-0 pb-16 pt-48 md:pt-52 w-full max-w-3xl min-h-screen">
+					<div className="flex flex-col gap-6 min-h-[35vh]">
+						{descriptionParagraphs.map((paragraph, index) => (
+							<p className="text-lg md:text-xl" key={index}>
+								{paragraph}
+							</p>
+						))}
+					</div>
+					<div className="sticky bottom-0 bg-sand py-4 md:py-6 z-10 ">
 						<div className="flex justify-center">
 							<Link
-								to={`${currentLocation.pathname}/stap-1`}
+								to={"1"}
 								className={`${styles.buttonBase} bg-red hover:bg-pink px-10 py-2`}>
 								Begin oefening
 							</Link>
@@ -37,3 +46,7 @@ function AssignmentDescriptionPage() {
 }
 
 export default AssignmentDescriptionPage;
+
+AssignmentDescriptionPage.propTypes = {
+	contentData: object,
+};
